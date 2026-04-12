471b7fb670682ea8ae753e9aaa029eb649cc9fbb
lfg-cdg <hellatriplexxx@gmail.com>
2026-04-12 16:38:38 +0300

    add get and post requests for tags, delete for notes

--- backend-roadmap/app/main.py
+++ backend-roadmap/app/main.py
@@ -1,13 +1,40 @@
 from fastapi import FastAPI
 
-from app.schemas import NoteCreate
+from app.schemas import NoteCreate, TagCreate
 
 app = FastAPI(title="Notes API", version="0.1.0")
+
 
 @app.get("/")
 def healthcheck():
     return {"status": "ok"}
 
+
 @app.post("/notes", status_code=201)
 def create_note(note: NoteCreate):
     return {"message": "Note created!", "note": note.model_dump()}
+
+
+@app.get("/notes")
+def check_notes():
+    return {"notes": []}
+
+
+@app.get("/notes/{note_id}")
+def check_note_by_id(note_id: int):
+    return {"note_id": note_id, "title": "Jopa"}
+
+
+@app.post("/tags", status_code=201)
+def create_tag(tag: TagCreate):
+    return {"message": "Tag created!", "tag": tag.model_dump()}
+
+
+@app.get("/tags")
+def check_tags():
+    return {"tags": []}
+
+
+@app.delete("/notes/{note_id}", status_code=200)
+def delete_note(note_id: int):
+    return {"message": "Note deleted!", "note_id": note_id}
--- backend-roadmap/app/schemas.py
+++ backend-roadmap/app/schemas.py
@@ -5,3 +5,7 @@ class NoteCreate(BaseModel):
     title: str
     content: str
     tags: list[str] = []
+
+
+class TagCreate(BaseModel):
+    name: str
