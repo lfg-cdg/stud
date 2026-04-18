7bdf83585d221947d2174ecabf5f8db5f415b790
lfg-cdg <hellatriplexxx@gmail.com>
2026-04-18 12:20:17 +0300

    find froblems

---
+++ backend-roadmap/app/create_task.py
@@ -0,0 +1,14 @@
+from app.database import SessionLocal
+from app.models import Task
+
+
+db = SessionLocal()
+
+task1 = Task(title = "Task1", is_done = True)
+
+db.add(task1)
+db.commit()
+db.refresh(task1)
+
+print(task1.id, task1.title)
+
