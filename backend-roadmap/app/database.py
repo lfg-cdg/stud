409b8131f8f79fc84a4c1d1787502b393987a4a5
lfg-cdg <hellatriplexxx@gmail.com>
2026-04-18 11:30:21 +0300

    fix DATABASE_URL

--- backend-roadmap/app/database.py
+++ backend-roadmap/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/backend_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
